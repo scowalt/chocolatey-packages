91bdac2daeda635539283f19e1b2b81011158475
Scott Walters <me@scowalt.com>
2022-03-05 18:19:18 -0800

    Updating depressurizer

---
+++ packages/depressurizer/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName   = 'depressurizer'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = 'https://github.com/Depressurizer/Depressurizer/releases/download/$env:chocolateyPackageVersion/Depressurizer-$env:chocolateyPackageVersion.zip'
+
+  softwareName  = 'Depressurizer*'
+
+  checksum = '178c3edf3d49f4e76b67e1ee9b9984eef21a079cbf1a732b99c7bc42a7eec762'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
