b791d8a1e42f495aa55482cdc9052aa64b039824
Scott Walters <me@scowalt.com>
2022-08-14 09:27:10 -0700

    New automatic update logic (#26)

---
+++ scripts/cryptomator-release.ts
@@ -0,0 +1,82 @@
+import path from "path";
+import {
+  createNupkgAndUpload,
+  getCurrentVersion,
+  getGithubReleaseInfo,
+  getSha256,
+  GithubReleaseInfo,
+  parseOptions,
+} from "./shared/release";
+import fs from "fs";
+
+const owner = "cryptomator";
+const repo = "cryptomator";
+const packageFolder = path.join(__dirname, "..", "packages", "cryptomator");
+const nuspecPath = path.join(packageFolder, "cryptomator.nuspec");
+const installPwshPath = path.join(
+  packageFolder,
+  "tools",
+  "chocolateyinstall.ps1"
+);
+
+async function main(): Promise<void> {
+  const options = parseOptions();
+  const current = getCurrentVersion(nuspecPath);
+  const latestRelease = await getGithubReleaseInfo(owner, repo);
+  const latestVersion = latestRelease.data.tag_name;
+  console.log("Latest version:", latestVersion);
+
+  if (current !== latestVersion) {
+    const downloadUrl = getDownloadUrl(latestRelease);
+    if (downloadUrl === null) {
+      console.log(`No .msi release for ${latestVersion}`);
+      return;
+    }
+
+    const sha256 = await getSha256(downloadUrl);
+    console.log("SHA256:", sha256);
+
+    updatePackage(downloadUrl, sha256, latestVersion);
+    await createNupkgAndUpload(packageFolder, options.upload);
+  } else {
+    console.log("No update available");
+  }
+}
+
+function updatePackage(
+  downloadUrl: string,
+  sha: string,
+  version: string
+): void {
+  let installPwsh = fs.readFileSync(installPwshPath, "utf8");
+  installPwsh = installPwsh.replace(
+    /\$url64bit \= .*/,
+    `$url64bit = "${downloadUrl}"`
+  );
+  installPwsh = installPwsh.replace(
+    /\$checksum64 \= .*/,
+    `$checksum64 = '${sha}'`
+  );
+  fs.writeFileSync(installPwshPath, installPwsh);
+  console.log("Updated chocolateyinstall.ps1");
+
+  let nuspec = fs.readFileSync(nuspecPath, "utf8");
+  nuspec = nuspec.replace(
+    /<version>.*<\/version>/,
+    `<version>${version}</version>`
+  );
+  fs.writeFileSync(nuspecPath, nuspec);
+  console.log("Updated nuspec");
+}
+
+function getDownloadUrl(release: GithubReleaseInfo): string | null {
+  const asset = release.data.assets.find((asset) =>
+    asset.name.endsWith("-x64.msi")
+  );
+  if (!asset) {
+    return null;
+  }
+  return asset.browser_download_url;
+}
+
+main();
---
+++ scripts/depressurizer-release.ts
@@ -0,0 +1,71 @@
+import { assert } from "ts-essentials";
+import path from "path";
+import {
+  createNupkgAndUpload,
+  getCurrentVersion,
+  getGithubReleaseInfo,
+  getSha256,
+  GithubReleaseInfo,
+  parseOptions,
+} from "./shared/release";
+import fs from "fs";
+
+const owner = "Depressurizer";
+const repo = "Depressurizer";
+const packageFolder = path.join(__dirname, "..", "packages", "depressurizer");
+const nuspecPath = path.join(packageFolder, "depressurizer.nuspec");
+const installPwshPath = path.join(
+  packageFolder,
+  "tools",
+  "chocolateyinstall.ps1"
+);
+
+async function main(): Promise<void> {
+  const options = parseOptions();
+  const current = getCurrentVersion(nuspecPath);
+  const latestRelease = await getGithubReleaseInfo(owner, repo);
+  const latestVersion = latestRelease.data.tag_name.replace("v", "");
+  console.log("Latest version:", latestVersion);
+
+  if (current !== latestVersion) {
+    const downloadUrl = getDownloadUrl(latestRelease);
+    const sha256 = await getSha256(downloadUrl);
+    console.log("SHA256:", sha256);
+    updatePackage(downloadUrl, sha256, latestVersion, current);
+    await createNupkgAndUpload(packageFolder, options.upload);
+  } else {
+    console.log("No update available");
+  }
+}
+
+function updatePackage(
+  downloadUrl: string,
+  sha: string,
+  version: string,
+  oldVersion: string
+): void {
+  let installPwsh = fs.readFileSync(installPwshPath, "utf8");
+  installPwsh = installPwsh.replace(/\$url \= .*/, `$url = "${downloadUrl}"`);
+  installPwsh = installPwsh.replace(/\$checksum \= .*/, `$checksum = '${sha}'`);
+  fs.writeFileSync(installPwshPath, installPwsh);
+  console.log("Updated chocolateyinstall.ps1");
+
+  let nuspec = fs.readFileSync(nuspecPath, "utf8");
+  nuspec = nuspec.replace(
+    /<version>.*<\/version>/,
+    `<version>${version}</version>`
+  );
+  nuspec = nuspec.replace(`v${oldVersion}`, `v${version}`);
+  fs.writeFileSync(nuspecPath, nuspec);
+  console.log("Updated nuspec");
+}
+
+function getDownloadUrl(release: GithubReleaseInfo): string {
+  const asset = release.data.assets.find((asset) =>
+    asset.name.endsWith(".exe")
+  );
+  assert(asset);
+  return asset.browser_download_url;
+}
+
+main();
