f1205ae5d1434c82bce12f417a28b8faf6557b88
scowalt <scowalt@users.noreply.github.com>
2024-11-20 04:11:08 +0000

    Automatic version updates

--- packages/cryptomator/tools/chocolateyInstall.ps1
+++ packages/cryptomator/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url64bit = "https://github.com/cryptomator/cryptomator/releases/download/1.14.0/Cryptomator-1.14.0-x64.msi"
-$checksum64 = '998b02c07f9df2bb661aee2c571be458a64c981da01e50d1857a6f7b892d67e4'
+$url64bit = "https://github.com/cryptomator/cryptomator/releases/download/1.14.2/Cryptomator-1.14.2-x64.msi"
+$checksum64 = '521f49f22f2c3f4722c2a5ef5e069b673b73963ecbe576b2f13687eb687b2d56'
 
 $packageArgs = @{
   packageName            = 'cryptomator'
