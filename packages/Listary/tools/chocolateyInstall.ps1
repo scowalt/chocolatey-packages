f8f8ea9c9474d02bfebb58206fc1626e34ada313
scowalt <scowalt@users.noreply.github.com>
2025-06-06 04:15:15 +0000

    Automatic version updates

--- packages/Listary/tools/chocolateyInstall.ps1
+++ packages/Listary/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $url = "https://www.listary.com/download/Listary.exe"
-$hash = '9eb1b926c4b68732f7da621f6f6fcbfce8919fc87f64d8f3ba091b01bbc3e8e2'
+$hash = '3a23a01c8fcc70298e9f6f9d7c070c88d1da5eb441bcb422ef97f86a5710eb71'
 
 Install-ChocolateyPackage `
 	-PackageName  'Listary' `
